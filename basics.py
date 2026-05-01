b926cbf5a1199cae0b6df305f7180a41a3db070d
sophie-rossiter <sophrossit@icloud.com>
2026-05-01 10:22:51 +0100

    Added docstrings for basics.py

--- basics.py
+++ basics.py
@@ -1,13 +1,16 @@
 def sum_list_of_numbers(numbers):
+    """Returns the sum of all numbers in a list"""
     total = 0
     for number in numbers:
         total += number
     return total
 
 def reverse_word(word):
+    """Returns the reverse of a given string"""
     return word[::-1]
 
 def get_highest_scorer(scores):
+    """Returns the name of the highest scorer in a dictionary of name/score pairs"""
     highest_score = 0
     highest_name = ""
     for name, score in scores.items():
@@ -17,6 +20,7 @@ def get_highest_scorer(scores):
     return highest_name
 
 def find_multiples_to_ten(num):
+    """Returns a list of the multiples of a number which are less than 10"""
     multiples_list = [num]
     current_number = num
     while current_number < 10:
@@ -26,9 +30,11 @@ def find_multiples_to_ten(num):
     return multiples_list
 
 def greeting(name):
+    """Returns a personalised greeting string for the given name"""
     return f"Hello, {name}! Welcome."
 
 def read_file(file):
+    """Reads and returns the contents of a file. Returns None if the file is not found."""
     try:
         with open(file, "r") as f:
             contents = f.read()
